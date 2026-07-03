7dc4e25a7e0fa8e4b85d747518d6b9e7223f80cc
enzodiasn-hueasd <enzodiasn@gmail.com>
2026-07-03 03:16:37 -0300

    discord-bot

--- recorde.py
+++ recorde.py
@@ -1,40 +1,48 @@
 import discord
 from discord.ext import commands, tasks
 import os
+import asyncio
 
 TOKEN = os.getenv("TOKEN")
 VOICE_CHANNEL_ID = 1367543413808955422
 
 intents = discord.Intents.default()
+intents.guilds = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
 async def join_voice():
-    channel = bot.get_channel(VOICE_CHANNEL_ID)
-    if channel is None:
-        print("Canal não encontrado")
-        return
+    await bot.wait_until_ready()
 
-    vc = discord.utils.get(bot.voice_clients)
-
-    # se não existe conexão OU desconectou de verdade
-    if vc is None or not vc.is_connected():
+    while not bot.is_closed():
         try:
-            await channel.connect(reconnect=True)
-            print("Entrou na call")
+            channel = bot.get_channel(VOICE_CHANNEL_ID)
+
+            if channel is None:
+                print("Canal não encontrado")
+                await asyncio.sleep(10)
+                continue
+
+            vc = discord.utils.get(bot.voice_clients, guild=channel.guild)
+
+            if vc is None or not vc.is_connected():
+                print("Entrando na call...")
+                await channel.connect()
+            else:
+                print("Já está na call")
+
         except Exception as e:
-            print("Erro ao entrar:", e)
+            print("Erro:", e)
+
+        await asyncio.sleep(10)
+
 
 @bot.event
 async def on_ready():
     print(f"Logado como {bot.user}")
-    await join_voice()
-    monitor.start()
 
-@tasks.loop(seconds=10)
-async def monitor():
-    vc = discord.utils.get(bot.voice_clients)
 
-    # se caiu da call → reconecta
-    if vc is None or not vc.is_connected():
-        print("Detectei queda da call, reconectando...")
-        await join_voice()
+bot.loop.create_task(join_voice())
+
+bot.run(TOKEN)
