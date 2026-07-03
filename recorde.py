a490672278d4c9976eb9f9a9a7109642148dfb7e
enzodiasn-hueasd <enzodiasn@gmail.com>
2026-07-03 03:32:11 -0300

    Update recorde.py

--- recorde.py
+++ recorde.py
@@ -8,41 +8,35 @@ VOICE_CHANNEL_ID = 1367543413808955422
 
 intents = discord.Intents.default()
 intents.guilds = True
+intents.voice_states = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-
-
-async def join_voice():
-    await bot.wait_until_ready()
-
-    while not bot.is_closed():
-        try:
-            channel = bot.get_channel(VOICE_CHANNEL_ID)
-
-            if channel is None:
-                print("Canal não encontrado")
-                await asyncio.sleep(10)
-                continue
-
-            vc = discord.utils.get(bot.voice_clients, guild=channel.guild)
-
-            if vc is None or not vc.is_connected():
-                print("Entrando na call...")
-                await channel.connect()
-            else:
-                print("Já está na call")
-
-        except Exception as e:
-            print("Erro:", e)
-
-        await asyncio.sleep(10)
 
 
 @bot.event
 async def on_ready():
     print(f"Logado como {bot.user}")
+    join_voice.start()
 
 
-bot.loop.create_task(join_voice())
+@tasks.loop(seconds=10)
+async def join_voice():
+    await bot.wait_until_ready()
+
+    channel = bot.get_channel(VOICE_CHANNEL_ID)
+
+    if channel is None:
+        print("Canal não encontrado")
+        return
+
+    vc = discord.utils.get(bot.voice_clients, guild=channel.guild)
+
+    if vc is None or not vc.is_connected():
+        try:
+            await channel.connect()
+            print("Entrou na call")
+        except Exception as e:
+            print("Erro ao entrar:", e)
+
 
 bot.run(TOKEN)
